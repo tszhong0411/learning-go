77b80ed8785a7b64d9974bb3975304a7296360d0
tszhong0411 <me@honghong.me>
2025-02-18 22:42:52 +0800

    feat: part 8

---
+++ 08_functions/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v \n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
+
+func cycleNames(n []string, f func(string)) {
+	for _, value := range n {
+		f(value)
+	}
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
+
+func main() {
+	sayGreeting("mario")
+	sayGreeting("luigi")
+	sayBye("mario")
+	sayBye("luigi")
+
+	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
+	cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
+
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
+
+	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f \n", a1, a2)
+}
